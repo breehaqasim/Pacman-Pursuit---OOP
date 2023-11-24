f84aa3c0e5f50beff76ee3ada0acfdae2d840a51
HammadMal <122615617+HammadMal@users.noreply.github.com>
2023-11-24 17:18:11 +0500

    Update Level1.hpp

--- Pacman Pursuit/Level1.hpp
+++ Pacman Pursuit/Level1.hpp
@@ -1,10 +1,9 @@
 #include<SDL.h>
 #include <SDL_mixer.h>
+#pragma once
 
-void drawObjects(SDL_Renderer* gRnderer, SDL_Texture* assets);
-// void createObject(int x, int y);
-
-struct Unit{
-SDL_Rect srcRect, moverRect;
+class Level1
+{
+    public:
+        void drawObjects(SDL_Renderer* gRnderer, SDL_Texture* assets);
 };
-

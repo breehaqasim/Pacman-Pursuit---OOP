514d97c9142783879ad711347e57e7ab5de10cf2
Ahtishamu <122293944+Ahtishamu@users.noreply.github.com>
2023-11-27 22:21:36 +0500

    Update unit.hpp

--- Pacman Pursuit/unit.hpp
+++ Pacman Pursuit/unit.hpp
@@ -1,1 +1,11 @@
+#include "unit.hpp"
+#include <SDL.h>
+#pragma once
 
+//Enemy inherits from Unit
+//it is also a "Base" class to Red,Blue,Orange,Pink Enemy Classes & an "Abstract" class
+
+class Enemy : public Unit {
+    public:
+        virtual void drawEnemy(SDL_Renderer* gRnderer, SDL_Texture* assets)=0;
+};

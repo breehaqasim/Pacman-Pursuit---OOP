4e0e56e42e1f32b0f47be2f958bfbebbc74fabe3
Ahtishamu <122293944+Ahtishamu@users.noreply.github.com>
2023-11-28 18:49:02 +0500

    Update red.hpp
    bug fix

--- Pacman Pursuit/red.hpp
+++ Pacman Pursuit/red.hpp
@@ -1,56 +1,13 @@
-#include "red.hpp"
-#include<iostream>
+#include "enemy.hpp"
+#include <SDL.h>
+#pragma once
 
-// Unit red= {{596,250,40,37}, {443, 228, 30, 30}};
-Unit red= {{596,250,40,37}, {258, 228, 30, 30}};
+//Red inherits from Enemy Class
 
-
-Unit red2= {{596,250,40,37}, {258, 228, 30, 30}};
-
-void Red::drawEnemy(SDL_Renderer* gRnderer, SDL_Texture* assets)
+class Red : public Enemy 
 {
-
-    bool flag = false;
-    SDL_RenderCopy(gRnderer, assets, &red.srcRect, &red.moverRect);
-
-    // std::cout<<red.moverRect.x<<std::endl;
-
-
-        if (red2.moverRect.x >= 899)
-        {
-            // SDL_RenderCopy(gRnderer, assets, &red.srcRect, &red.moverRect);
-            red.moverRect.x -= 2;
-            std::cout<<red.moverRect.x<<"check"<<std::endl;
-
-
-            if (red.moverRect.x == 258)
-            {
-                red2.moverRect.x=258;
-            }
-
-
-        }
-        else if (red2.moverRect.x < 900)
-        {
-            //SDL_RenderCopy(gRnderer, assets, &red.srcRect, &red.moverRect);
-            red.moverRect.x += 2;
-            std::cout<<red.moverRect.x<<"check"<<std::endl;
-
-            if (red.moverRect.x==900)
-            {
-                red2.moverRect.x=899;
-            }
-
-        }
-
-    // Draw the enemy
-    // SDL_RenderCopy(gRnderer, assets, &red.srcRect, &red.moverRect);
-
-
-    // Update the screen
-    // SDL_RenderPresent(gRnderer);
-
-
-}
+    public:
+        virtual void drawEnemy(SDL_Renderer* gRnderer, SDL_Texture* assets) override;
+};
 
 

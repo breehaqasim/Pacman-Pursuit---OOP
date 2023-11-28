c540c045571c01447952373ca30cc667888a808e
Ahtishamu <122293944+Ahtishamu@users.noreply.github.com>
2023-11-28 18:50:14 +0500

    Update orange.hpp
    added class definition

--- Pacman Pursuit/orange.hpp
+++ Pacman Pursuit/orange.hpp
@@ -1,1 +1,11 @@
+#include "enemy.hpp"
+#include <SDL.h>
+#pragma once
 
+//Orange inherits from Enemy Class
+
+class Orange : public Enemy 
+{
+    public:
+        virtual void drawEnemy(SDL_Renderer* gRnderer, SDL_Texture* assets) override;
+};

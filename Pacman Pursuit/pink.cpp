db42a0f43168c79c7edd411a901227634947ae21
Breeha Qasim <75079166+breehaqasim@users.noreply.github.com>
2023-11-29 13:18:14 +0500

    Update pink.cpp

--- Pacman Pursuit/pink.cpp
+++ Pacman Pursuit/pink.cpp
@@ -1,1 +1,27 @@
+#include "pink.hpp"
+#include <iostream>
 
+Unit pink= {{439,250,39,37}, {701, 228, 30, 30}};
+Unit pink2= {{439,250,39,37}, {701, 228, 30, 30}};
+
+void Pink::drawEnemy(SDL_Renderer* gRnderer, SDL_Texture* assets)
+{
+
+    SDL_RenderCopy(gRnderer, assets, &pink.srcRect, &pink.moverRect);
+        if (pink2.moverRect.y >= 382)
+        {
+            pink.moverRect.y -= 2;
+            if (pink.moverRect.y == 228)
+            {
+                pink2.moverRect.y =228;
+            }
+        }
+        else if (pink2.moverRect.y < 383 )
+        {
+            pink.moverRect.y += 2;
+            if (pink.moverRect.y == 382)
+            {
+                pink2.moverRect.y=382;
+            }
+        }
+}

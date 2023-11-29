8de1e2ad000ee24461160484d7965e9f55362777
Breeha Qasim <75079166+breehaqasim@users.noreply.github.com>
2023-11-29 13:17:58 +0500

    Update pink.hpp

--- Pacman Pursuit/pink.hpp
+++ Pacman Pursuit/pink.hpp
@@ -1,1 +1,11 @@
+#include "enemy.hpp"
+#include <SDL.h>
+#pragma once
 
+//Pink inherits from Enemy Class
+
+class Pink : public Enemy 
+{
+    public:
+        virtual void drawEnemy(SDL_Renderer* gRnderer, SDL_Texture* assets) override;
+};

60f99797ebe7df412b455d09df97a05d84b98074
HammadMal <122615617+HammadMal@users.noreply.github.com>
2023-12-05 11:48:50 +0500

    Update purple.hpp

--- Pacman Pursuit/purple.hpp
+++ Pacman Pursuit/purple.hpp
@@ -1,1 +1,11 @@
+#include "enemy.hpp"
+#include <SDL.h>
+#pragma once
 
+//Purple inherits from Enemy Class
+
+class Purple : public Enemy 
+{
+    public:
+        virtual void drawEnemy(SDL_Renderer* gRnderer, SDL_Texture* assets) override;
+};

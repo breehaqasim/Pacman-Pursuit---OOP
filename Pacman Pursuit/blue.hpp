f1a82490e744fe7d679079dc6d2f7681bc8b8382
HammadMal <122615617+HammadMal@users.noreply.github.com>
2023-12-05 11:47:05 +0500

    Update blue.hpp

--- Pacman Pursuit/blue.hpp
+++ Pacman Pursuit/blue.hpp
@@ -1,1 +1,11 @@
+#include "enemy.hpp"
+#include <SDL.h>
+#pragma once
 
+//Pink inherits from Enemy Class
+
+class Blue : public Enemy 
+{
+    public:
+        virtual void drawEnemy(SDL_Renderer* gRnderer, SDL_Texture* assets) override;
+};

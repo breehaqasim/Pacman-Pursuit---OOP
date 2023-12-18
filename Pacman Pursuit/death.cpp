82553eeb7f5038729325246a104dcf38ed2779fc
Breeha Qasim <75079166+breehaqasim@users.noreply.github.com>
2023-12-18 14:44:47 +0500

    Update death.cpp

--- Pacman Pursuit/death.cpp
+++ Pacman Pursuit/death.cpp
@@ -1,1 +1,26 @@
+#include "death.hpp"
+#include <iostream>
+#include <SDL_ttf.h>
+#include <SDL.h>
+#include "game.hpp"
+#pragma once
 
+Death::Death()
+{
+    value=0;
+}
+
+void Death::operator ++()
+{
+    ++value;
+}
+
+void Death::display()
+{
+    std::cout<<value<<std::endl;
+}
+
+int Death::valueget()
+{
+    return value;
+}

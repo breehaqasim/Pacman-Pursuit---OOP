9ad8fa4d16fb9b088a5f33d59818794b26d9bc2e
Breeha Qasim <75079166+breehaqasim@users.noreply.github.com>
2023-12-18 14:42:07 +0500

    Update Level1.hpp

--- Pacman Pursuit/Level1.hpp
+++ Pacman Pursuit/Level1.hpp
@@ -1,9 +1,22 @@
 #include<SDL.h>
 #include <SDL_mixer.h>
+#include <vector>
+#include "pacman.hpp"
+#include "pink.hpp"
+#include "red.hpp"
+#include "purple.hpp"
+#include "orange.hpp"
+#include "blue.hpp"
+#include "death.hpp"
+#include "dot.hpp"
+#include "game.hpp"
 #pragma once
 
 class Level1
 {
+    private:
+        Unit arrow= {{281,401,324,236}, {1000, 100, 120, 120}};
+        Death deathCounter;
     public:
-        void drawObjects(SDL_Renderer* gRnderer, SDL_Texture* assets);
+        void drawObjects(SDL_Renderer* gRnderer, SDL_Texture* assets,std::vector<Enemy*>& enemies, Pacman& pac,Dot& dot, Death& death);
 };

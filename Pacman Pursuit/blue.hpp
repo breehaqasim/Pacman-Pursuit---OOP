7b69de3b5d2d65179c38724feeba32ff729d475d
Breeha Qasim <75079166+breehaqasim@users.noreply.github.com>
2023-12-18 14:45:06 +0500

    Update blue.hpp

--- Pacman Pursuit/blue.hpp
+++ Pacman Pursuit/blue.hpp
@@ -2,10 +2,14 @@
 #include <SDL.h>
 #pragma once
 
-//Pink inherits from Enemy Class
+//Blue inherits from Enemy Class
 
 class Blue : public Enemy 
 {
+    private:
+        Unit blue= {{519,250,41,37}, {400, 228, 30, 30}};
+        Unit blue2= {{519,250,41,37}, {400, 228, 30, 30}};
     public:
         virtual void drawEnemy(SDL_Renderer* gRnderer, SDL_Texture* assets) override;
+        Unit& Bluegetter();
 };

39fc9638aa69fb2fb11a5eb477196dd76976acb2
HammadMal <122615617+HammadMal@users.noreply.github.com>
2023-12-18 18:21:07 +0500

    Update pink.hpp
    updated pink

--- Pacman Pursuit/pink.hpp
+++ Pacman Pursuit/pink.hpp
@@ -6,6 +6,10 @@
 
 class Pink : public Enemy 
 {
+    private:
+        Unit pink= {{439,250,39,37}, {701, 228, 30, 30}};
+        Unit pink2= {{439,250,39,37}, {701, 228, 30, 30}};
     public:
         virtual void drawEnemy(SDL_Renderer* gRnderer, SDL_Texture* assets) override;
+        Unit& pinkgetter();
 };

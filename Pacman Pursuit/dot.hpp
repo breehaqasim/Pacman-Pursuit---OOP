97151312e91debab9c32864ac6d86f294403cab4
Ahtishamu <122293944+Ahtishamu@users.noreply.github.com>
2023-12-18 19:10:30 +0500

    Update dot.hpp

--- Pacman Pursuit/dot.hpp
+++ Pacman Pursuit/dot.hpp
@@ -1,1 +1,22 @@
+#include "unit.hpp"
+#pragma once
+#include <SDL.h>
+#include <vector>
+#include "game.hpp"
+#include "pacman.hpp"
+#include <SDL_ttf.h>
 
+class Dot 
+{
+    private:
+        bool flag=false;
+        std::vector<Unit> dots;
+        int score = 0;
+        int checkval=0;
+    public:
+        Dot();
+        void initializeDots();
+        int drawDot(SDL_Renderer* gRnderer, SDL_Texture* assets, Pacman& pac);
+        int scoregetter();
+        int checkvalgetter();
+};

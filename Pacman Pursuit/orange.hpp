05d7342d5bf86c085883f822f0142e457963bad3
HammadMal <122615617+HammadMal@users.noreply.github.com>
2023-12-18 18:25:59 +0500

    Update orange.hpp

--- Pacman Pursuit/orange.hpp
+++ Pacman Pursuit/orange.hpp
@@ -6,6 +6,10 @@
 
 class Orange : public Enemy 
 {
+    private:
+        Unit orange= {{353,250,41,34}, {258, 302, 30, 30}};
+        Unit orange2= {{353,250,41,34}, {258, 302, 30, 30}};
     public:
         virtual void drawEnemy(SDL_Renderer* gRnderer, SDL_Texture* assets) override;
+        Unit& Orangegetter();
 };

2eab01b6e42811c6d587cd1deb911436b98740fe
Ahtishamu <122293944+Ahtishamu@users.noreply.github.com>
2023-12-18 19:05:38 +0500

    Update unit.hpp

--- Pacman Pursuit/unit.hpp
+++ Pacman Pursuit/unit.hpp
@@ -1,8 +1,11 @@
 #include <SDL.h>
 #pragma once
+
 //this is Base class
+
 class Unit 
 {
     public:
         SDL_Rect srcRect, moverRect;
+        bool checkCollision(const Unit& other) const;
 };

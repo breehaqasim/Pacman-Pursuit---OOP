59ecb3f0ef7ed62327bb4d5afb767d5a3d9ea306
HammadMal <122615617+HammadMal@users.noreply.github.com>
2023-12-18 18:24:32 +0500

    Update pacman.hpp

--- Pacman Pursuit/pacman.hpp
+++ Pacman Pursuit/pacman.hpp
@@ -6,7 +6,12 @@
 
 class Pacman : public Unit 
 {
+    private:
+        Unit p = {{305,174,37,40}, {310, 174, 30, 30}};
     public:
         void movePacman(SDL_Keycode key);
-        void drawPacman(SDL_Renderer* gRnderer, SDL_Texture* assets);
+        void drawPacman(SDL_Renderer* gRnderer, SDL_Texture* assets); 
+        Unit& pacgetter();
+        Unit& pacresetter( Unit& p );
+        void freezepacman(SDL_Keycode key);
 };

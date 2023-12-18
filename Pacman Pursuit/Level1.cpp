81e183f889ec1d1b3b8e8b9e2ae4b1eee64950cc
Breeha Qasim <75079166+breehaqasim@users.noreply.github.com>
2023-12-18 14:42:36 +0500

    Update Level1.cpp

--- Pacman Pursuit/Level1.cpp
+++ Pacman Pursuit/Level1.cpp
@@ -1,62 +1,84 @@
+#include <iostream>
 #include "Level1.hpp"
 #include <vector>
 #include "SDL.h"
-#include "pacman.hpp"
-#include "red.hpp"
-
+#pragma once
 using namespace std;
 
-Unit orange= {{353,250,41,34}, {443, 260, 30, 30}};
-Unit pink= {{439,250,39,37}, {701, 328, 30, 30}};
-Unit blue= {{519,250,41,37}, {701, 360, 30, 30}};
 
-Unit dot1= {{394,185,18,18}, {570, 256, 20, 20}};
-Unit dot2= {{394,185,18,18}, {599, 256, 20, 20}};
-Unit dot3= {{394,185,18,18}, {627, 256, 20, 20}};
-Unit dot4= {{394,185,18,18}, {656, 256, 20, 20}};
+void Level1::drawObjects(SDL_Renderer* gRenderer, SDL_Texture* assets,std::vector<Enemy*>& enemies, Pacman& pac,Dot& dot,Death& death)
+{
+    Game a;
 
-Unit dot5= {{394,185,18,18}, {277, 325, 20, 20}};
-Unit dot6= {{394,185,18,18}, {314, 325, 20, 20}};
-Unit dot7= {{394,185,18,18}, {277, 359, 20, 20}};
-Unit dot8= {{394,185,18,18}, {314, 359, 20, 20}};
+    //drawing arrow keys to screen
+    SDL_RenderCopy(gRenderer, assets, &arrow.srcRect, &arrow.moverRect);
 
-Unit dot9= {{394,185,18,18}, {588, 359, 20, 20}};
-// First rectangle is srcRect, second is moverRect
-// these values are taken from the corresponding image in assets file
-// we've used spritecow.com to find exact values of other asset images
-
-
-void Level1::drawObjects(SDL_Renderer* gRenderer, SDL_Texture* assets){ 
-
-    Pacman pac;
+    // this function is drawing one pacman only right now
     pac.drawPacman(gRenderer, assets);  
 
-    Red red;
-    red.drawEnemy(gRenderer, assets);
+    //iterating over enemies to draw all of them
+    for (auto enemy : enemies) {
+        enemy->drawEnemy(gRenderer, assets);
 
-    //this function is drawing orange enemy
-    SDL_RenderCopy(gRenderer, assets, &orange.srcRect, &orange.moverRect);  
-    orange.moverRect.y+=1; 
+        if (Red* redEnemy = dynamic_cast<Red*>(enemy)) {
+            // Access the Red object and check collision
+            if (pac.pacgetter().checkCollision(redEnemy->redgetter())) {
+                pac.pacresetter(pac.pacgetter());
+                cout<<"Red collision"<<endl;
+                ++death;
+                int ab =1;
+                death.display();
+            }
+        }
 
-    //this function is drawing pink enemy
-    SDL_RenderCopy(gRenderer, assets, &pink.srcRect, &pink.moverRect);  
-    pink.moverRect.y+=1;
+        if (Blue* BlueEnemy = dynamic_cast<Blue*>(enemy)) {
+            // Access the Red object and check collision
+            if (pac.pacgetter().checkCollision(BlueEnemy->Bluegetter())) {
+                pac.pacresetter(pac.pacgetter());
+                cout<<"Blue collision"<<endl;
+                ++death;
+                death.display();
+            }
+        }
 
-    //this function is drawing blue enemy
-    SDL_RenderCopy(gRenderer, assets, &blue.srcRect, &blue.moverRect);  
-    blue.moverRect.y+=1;
+        if (Orange* OrangeEnemy = dynamic_cast<Orange*>(enemy)) {
+            // Access the Red object and check collision
+            if (pac.pacgetter().checkCollision(OrangeEnemy->Orangegetter())) {
+                pac.pacresetter(pac.pacgetter());
+                cout<<"Orange collision"<<endl;
+                ++death;
+                death.display();
+            }
+        }
 
-    //this function is drawing dots
-    SDL_RenderCopy(gRenderer, assets, &dot1.srcRect, &dot1.moverRect);
-    SDL_RenderCopy(gRenderer, assets, &dot2.srcRect, &dot2.moverRect);
-    SDL_RenderCopy(gRenderer, assets, &dot3.srcRect, &dot3.moverRect);
-    SDL_RenderCopy(gRenderer, assets, &dot4.srcRect, &dot4.moverRect);
+        if (Pink* PinkEnemy = dynamic_cast<Pink*>(enemy)) {
+            // Access the Red object and check collision
+            if (pac.pacgetter().checkCollision(PinkEnemy->pinkgetter())) {
+                pac.pacresetter(pac.pacgetter());
+                cout<<"Pink collision"<<endl;
+                ++death;
+                death.display();
+            }
+        }
 
-    SDL_RenderCopy(gRenderer, assets, &dot5.srcRect, &dot5.moverRect);
-    SDL_RenderCopy(gRenderer, assets, &dot6.srcRect, &dot6.moverRect);
-    SDL_RenderCopy(gRenderer, assets, &dot7.srcRect, &dot7.moverRect);
-    SDL_RenderCopy(gRenderer, assets, &dot8.srcRect, &dot8.moverRect);
+        if (Purple* PurpleEnemy = dynamic_cast<Purple*>(enemy)) {
+            // Access the Red object and check collision
+            if (pac.pacgetter().checkCollision(PurpleEnemy->purplegetter())) {
+                pac.pacresetter(pac.pacgetter());
+                cout<<"Purple collision"<<endl;
+                ++death;
+                death.display();
+            }
+        }
 
-    SDL_RenderCopy(gRenderer, assets, &dot9.srcRect, &dot9.moverRect);
+        string deathText = "Deaths: " + to_string(death.valueget());
 
+        Unit pacvoid=  {{305,174,37,40}, {310, 174, 30, 30}};
+        
+        a.renderText(gRenderer, deathText, 500, 500);
+        // a.renderText(gRenderer, deathText, 500, 500);
+    }
+
+    // this function is drawing dots right now
+    dot.drawDot(gRenderer, assets, pac);
 }

5ed6da4e0ff74b4ced1915959bb6bc856f7f6daa
Ahtishamu <122293944+Ahtishamu@users.noreply.github.com>
2023-12-18 19:08:40 +0500

    Update purple.hpp

--- Pacman Pursuit/purple.hpp
+++ Pacman Pursuit/purple.hpp
@@ -3,9 +3,12 @@
 #pragma once
 
 //Purple inherits from Enemy Class
-
 class Purple : public Enemy 
 {
+    private:
+        Unit purple1= {{663,249,40,38}, {258, 375, 30, 30}};
+        Unit purple2= {{596,250,40,37}, {258, 375 ,30, 30}};
     public:
         virtual void drawEnemy(SDL_Renderer* gRnderer, SDL_Texture* assets) override;
+        Unit& purplegetter();
 };

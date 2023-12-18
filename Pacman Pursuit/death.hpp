9903a23152bf3f06402dc30c13cfcd21b5520b96
Breeha Qasim <75079166+breehaqasim@users.noreply.github.com>
2023-12-18 14:44:20 +0500

    Update death.hpp

--- Pacman Pursuit/death.hpp
+++ Pacman Pursuit/death.hpp
@@ -1,1 +1,16 @@
+#include <SDL.h>
+#pragma once
+#include <SDL_ttf.h>
+#include "game.hpp"
 
+class Death
+{
+    private:
+        int value;
+    public:
+        void operator++();
+        void display();
+        Death();
+
+        int valueget();
+};

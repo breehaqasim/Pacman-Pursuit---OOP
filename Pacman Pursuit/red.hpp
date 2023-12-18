c392bbed3e27d0d5570a784ccb6034a8675b4c23
Ahtishamu <122293944+Ahtishamu@users.noreply.github.com>
2023-12-18 19:06:52 +0500

    Update red.hpp

--- Pacman Pursuit/red.hpp
+++ Pacman Pursuit/red.hpp
@@ -3,11 +3,13 @@
 #pragma once
 
 //Red inherits from Enemy Class
-
 class Red : public Enemy 
 {
+    private:
+        Unit red= {{596,250,40,37}, {258, 228, 30, 30}};
+        Unit red2= {{596,250,40,37}, {258, 228, 30, 30}};
     public:
         virtual void drawEnemy(SDL_Renderer* gRnderer, SDL_Texture* assets) override;
+        Unit& redgetter();
 };
-
 

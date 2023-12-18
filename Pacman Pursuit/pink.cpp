88896b72a919c0c60700df58f8f08880b3fdc38f
HammadMal <122615617+HammadMal@users.noreply.github.com>
2023-12-18 18:21:46 +0500

    Update pink.cpp
    updated pink

--- Pacman Pursuit/pink.cpp
+++ Pacman Pursuit/pink.cpp
@@ -1,27 +1,29 @@
 #include "pink.hpp"
 #include <iostream>
-
-Unit pink= {{439,250,39,37}, {701, 228, 30, 30}};
-Unit pink2= {{439,250,39,37}, {701, 228, 30, 30}};
+#pragma once
 
 void Pink::drawEnemy(SDL_Renderer* gRnderer, SDL_Texture* assets)
 {
-
     SDL_RenderCopy(gRnderer, assets, &pink.srcRect, &pink.moverRect);
-        if (pink2.moverRect.y >= 382)
+    if (pink2.moverRect.y >= 382)
+    {
+        pink.moverRect.y -= 2;
+        if (pink.moverRect.y == 228)
         {
-            pink.moverRect.y -= 2;
-            if (pink.moverRect.y == 228)
-            {
-                pink2.moverRect.y =228;
-            }
+            pink2.moverRect.y =228;
         }
-        else if (pink2.moverRect.y < 383 )
+    }
+    else if (pink2.moverRect.y < 383 )
+    {
+        pink.moverRect.y += 2;
+        if (pink.moverRect.y == 382)
         {
-            pink.moverRect.y += 2;
-            if (pink.moverRect.y == 382)
-            {
-                pink2.moverRect.y=382;
-            }
+            pink2.moverRect.y=382;
         }
+    }
 }
+
+Unit& Pink :: pinkgetter()
+    {
+        return pink;
+    }
